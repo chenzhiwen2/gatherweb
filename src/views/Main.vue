fe4fa25c2e5d19d9a03d98b855fc412b7bdc15b3
陈志文 <2206300923@qq.com>
2017-10-18 11:34:49 +0800

    init

---
+++ src/views/Main.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+    main
+		 	<el-button type="primary" @click="mu" >获取</el-button>
+			<br>
+      <el-button type="button" v-show="extent > 0 ? true: false"> 我是二级权限</el-button>
+			<br>
+			<img src="https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3" alt="">
+			<img src="https://wpimg.wallstcn.com/007ef517-bafd-4066-aae4-6883632d9646" alt="">
+
+
+	</div>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			extent: ''
+		}
+	},
+	components: {
+	},
+	methods: {
+    mu(){
+//			msgData().then(function (response) {
+//				console.log(response);
+//			 }).catch(function (error) {
+//				console.log(error);
+//			 })
+		},
+		getExtent() {
+			this.extent = sessionStorage.getItem('extent')
+		}
+	},
+	mounted() {
+		this.getExtent();
+	}
+}
+
+</script>
+
+<style>
+
+</style>
